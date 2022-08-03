86f6f7459d87c1bc41db7047efe1d604192a4c4c
MegsNaf <72550528+MegsNaf@users.noreply.github.com>
2022-08-03 21:18:58 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+require(`dotenv`)//env tanimliyoruz
+
+
+module.exports = {
+    dsc: {
+        token: "NzgwMzk3OTI3MzM5MjYxOTU0.G9pJwo.lYVwIZ0hT5hhLgZWLnV1jPrJxUmZOqvXur5gzc"
+    },
+
+    app: {
+        prefix: '!'
+    }
+};
---
+++ events/ready.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+const moment = require('moment');
+
+
+module.exports = client => {
+var oyun = [
+        `!test | !setup`,
+        `Ace Dev Testing `
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "" );
+        }, 2 * 4000 );
+  
+  client.user.setStatus("dnd");
+}; 
